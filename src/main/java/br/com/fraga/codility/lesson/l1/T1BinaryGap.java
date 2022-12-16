d749d5c314250a3035025d9c5dd9e988b82302e5
thiagofraga <thiagofraga.lm@gmail.com>
2022-12-16 10:25:23 -0300

    codility

---
+++ src/main/java/br/com/fraga/codility/lesson/l1/T1BinaryGap.java
@@ -0,0 +1,10 @@
+package br.com.fraga.codility.lesson.l1;
+
+class T1BinaryGap {
+
+    public int solution(int N) {
+        // Implement your solution here
+        return N;
+    }
+
+}
---
+++ src/test/java/br/com/fraga/codility/lesson/l1/T1BinaryGapTest.java
@@ -0,0 +1,16 @@
+package br.com.fraga.codility.lesson.l1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class T1BinaryGapTest {
+
+    @Test
+    public void test1() {
+        assumeTrue(5 > 1);
+        assertEquals(5 + 2, 7);
+    }
+
+}
